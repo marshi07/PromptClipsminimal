f5f0eef189f8104ca32656b89f3072c5b087c75d
marshi07 <marshikons@gmail.com>
2025-05-02 10:12:09 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,15 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>PromptClips - It Works!</title>
+      </Head>
+      <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: '#111', color: '#fff' }}>
+        <h1>PromptClips is working!</h1>
+      </main>
+    </>
+  )
+}
