15f7edfc99f9528fabc5c779c33d28b2608c19e1
marshi07 <marshikons@gmail.com>
2025-05-02 10:20:57 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#111', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>PromptClips is working!</h1>
+    </div>
+  )
+}
