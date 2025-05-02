b9c931e8755a284130e573623f6276467aa511f7
marshi07 <marshikons@gmail.com>
2025-05-02 10:32:41 +0530

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ backgroundColor: '#111', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>PromptClips is working!</h1>
-    </div>
-  )
-}
